d54a5b3782f0fd23f6f5a37b605f51d734d39b06
Amir Sanni <amirsanni@gmail.com>
2019-04-08 17:07:08 +0100

    Update Master.php

--- src/Master.php
+++ src/Master.php
@@ -13,7 +13,7 @@ class Master {
     ********************************************************************************************************************************
     */
 
-    protected function curl($url, $use_post, $post_data=[]){
+    protected function curl($url, $use_post, $post_data=[], $use_custom=FALSE, $custom_type=''){
         $curl = curl_init();
         
         $headers = [
@@ -27,6 +27,11 @@ class Master {
         
         if($use_post){
             curl_setopt($curl, CURLOPT_POST, TRUE);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));
+        }
+
+        else if($use_custom){
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $custom_type);
             curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));
         }
         
